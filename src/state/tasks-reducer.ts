679c33d1d148b6a107b6dea897730b9ca2c360fc
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-09-14 19:31:37 +0300

    reducer for tasks - add task

---
+++ src/state/tasks-reducer.ts
@@ -0,0 +1,34 @@
+import {FilterValuesType, TasksStateType, TodolistType} from '../App';
+import {v1} from 'uuid';
+
+
+export type FirstActionType = ReturnType<typeof removeTaskAC>
+export type SecondActionType = ReturnType<typeof addTaskAC>
+
+type ActionsType = FirstActionType | SecondActionType
+
+export const tasksReducer = (state: TasksStateType, action: ActionsType) => {
+    switch (action.type) {
+        case 'REMOVE-TASK':
+            return {
+                ...state,
+                [action.todolistId]: state[action.todolistId].filter(task => task.id !== action.taskId)
+            }
+        case "ADD-TASK":
+            let newTask = {id: v1(), title:action.title, isDone: false}
+            return {...state, [action.todolistID]:[newTask, ...state[action.todolistID]]}
+        default:
+            return state
+    }
+}
+
+export const removeTaskAC = (taskId: string, todolistId: string) => {
+    return { type: 'REMOVE-TASK', taskId, todolistId} as const
+}
+export const addTaskAC = (title: string, todolistID: string) => (
+    {
+        type: "ADD-TASK",
+        title,
+        todolistID
+    } as const
+)
